df5a7b600e330016d3a5f9697ba650d141122a8c
natewong <nwwong17@gmail.com>
2021-09-11 12:47:50 -0700

    Used loops to create math functions for APCS HW

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,36 +1,53 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class MyMain {
 
     // Calculates the factorial of a number
     public static double factorial(int x) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        double ans = 1;
+        for(double i=1; i<x+1; i++)
+        {
+            ans=ans*i;
+        }
+        return ans;
     }
-
     // Calculates the value for the math constant e iteratively
     // goes until it calculates an answer less than 0.00000000001 
     // different from the value Math.E
     public static double calculateE() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        double evalue = 0;
+        for(int i=0; Math.abs(evalue-Math.E)>0.00000000001; i++)
+        {
+            evalue+=1/factorial(i);
+        }
+
+        return evalue;
     }
 
     // Calculates the square root iteratively, using the Babylonian method
     public static double babylonian(double x) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        double n = 10;
+        double m = 0;
+        for(int i=0; i<500; i++)
+        {
+            m = (n+x/n)/2;
+            if(Math.abs(m-n)<0.00001)
+            {
+                break;
+            }
+            n = m;
+        }
+        return m;
     }
     
     
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-        // YOUR CODE HERE
-
-
-        
-
+        System.out.println("Enter a positive number.");
+        int number = scan.nextInt();
+        System.out.println("The square root of your number is approximately " + babylonian(number));
+        System.out.println("The value of e is approximately " + calculateE());
         scan.close();
     }
 }
